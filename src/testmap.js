89be10a128bb84d74d5bbba8a4964978b87430f5
nurbxfit <root.toor97@gmail.com>
2020-11-02 16:32:38 +0800

    added testmap; trying map instead of loop; because problem with event limit

---
+++ src/testmap.js
@@ -0,0 +1,94 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const scripts = require('./scripts/index');
+const {URI,URI2} = require('./configs/mongo');
+const errorHandler = require('./lib/errorHandler');
+const ArticleSchema = require('./models/articles');
+const {mergeCollection} = require('./lib/mergedb');
+const _log = require('./lib/log');
+
+const app = {};
+
+const init = async () => {
+  const connection = {};
+  try{
+    connection[process.env.MONGO_DB_NAME] = await mongoConnect(URI,process.env.MONGO_DB_NAME);
+    connection[process.env.MONGO2_DB_NAME] = await mongoConnect(URI2,process.env.MONGO2_DB_NAME);
+    console.log('Created connection');
+    return Promise.resolve(connection);
+  }catch(error){
+    return Promise.reject(error);
+  }
+}
+
+const mongoConnect = async (URI,connectionName) =>{
+ try{
+   const connection = await mongoose.createConnection(URI); 
+   console.log('Created Connection:',connectionName);
+   return Promise.resolve(connection);
+ }catch(error){
+    console.error('Error Creating Connection:',error);
+    return Promise.reject(error);
+ } 
+}
+
+app.crawl = async () =>{ 
+  const con1 = app.connection[process.env.MONGO_DB_NAME];
+  const errors = [];
+ 
+    try{
+      await Promise.all(Object.values(scripts).map(async (site)=>{
+        try{
+          console.log('Crawling..:',site);
+          site.Model = con1.model(site.name,ArticleSchema);
+          await site.crawl();
+        }catch(error){
+          //doing comething with error;
+          console.log('Adoiiiii...');
+        }
+           
+      }));
+      //for ( let i = 0; i < Object.values(scripts).length; ){
+        //const site = Object.values(scripts)[i];
+        //console.log('Crawling..:',site);
+        //site.Model = con1.model(site.name,ArticleSchema);
+        //await site.crawl();
+        //i++;
+      //}
+    }catch(error){
+      console.log('Error while crawling...');
+      console.log('Error:',error);
+      errors.push(error);
+    }finally{
+      //await mergeCollection(app.connection); 
+      console.log('Done maybe .. ?');
+    } 
+}
+
+
+app.run = async () =>{
+  try{
+    app.connection = await init();
+    console.log('received connection..');
+    await app.crawl();
+    console.log('Finished crawling');
+    await mergeCollection(app.connection);
+  }catch(error){
+    console.error('We Found Error...');
+    console.error('Error are:',error);
+    //process.exit(1);
+  }finally{
+    const filename = 'finished';
+    const payload = JSON.stringify({status:'finished...',time: new Date()});
+    _log.append(filename,payload,function(error){
+      if(error){
+        console.log('Error wrinting logs');
+        process.exit(0);
+      }
+    });
+    process.exit(0);
+  } 
+}
+
+app.run();
+
