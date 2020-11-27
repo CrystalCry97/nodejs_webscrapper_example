e4b48c90858755f9a8f34e3e00619efa33d830cb
nurbxfit <root.toor97@gmail.com>
2020-11-27 21:59:35 +0800

    Edit: scripts/index to use all

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,15 @@
 const path = require('path');
 
 const scripts = {}; 
-scripts['acspub'] = require('./scrapper/acspub');
+//scripts['acspub'] = require('./scrapper/acspub');
 
-//const folderPath = path.join(__dirname,'scrapper');
-//require('fs').readdirSync(folderPath).forEach(function(file){
-  //if(file.match(/\.js$/) !== null ){
-    //const name = file.replace('.js',''); 
-    //scripts[name] = require('./scrapper/'+file);
-  //}
+const folderPath = path.join(__dirname,'scrapper');
+require('fs').readdirSync(folderPath).forEach(function(file){
+  if(file.match(/\.js$/) !== null ){
+    const name = file.replace('.js',''); 
+    scripts[name] = require('./scrapper/'+file);
+  }
 
-//});
+});
 module.exports = scripts;
 
