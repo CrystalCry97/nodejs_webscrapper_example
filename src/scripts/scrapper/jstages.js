918ce50d783f9d5d5ac2bd0735e2bfc97c96ecfe
nurbxfit <root.toor97@gmail.com>
2020-11-29 00:26:53 +0800

    added title validation to jstages

--- src/scripts/scrapper/jstages.js
+++ src/scripts/scrapper/jstages.js
@@ -1,7 +1,7 @@
 const puppeteer     = require('puppeteer');
 const cheerio       = require('cheerio');
 const {promisify}   = require('util');
-const {getHTML,insertDB,insertErrorHandler,sleep}       = require('../../lib/crawler');
+const {getHTML,insertDB,insertErrorHandler,sleep,trimText,cleanTitle,validateTitle}       = require('../../lib/crawler');
 const keywords = require('../../lib/keywords').load();
 
 //const keywords = [
@@ -143,6 +143,11 @@ const getArticleFromHTML = (html,url)=>{
     const link = url;
     let title = $(selectors.title).text();
     title = (title === "&nbsp;")||(title === "&nbsp" )? $(selectors.subtitle).attr('content') : title;
+    title = await cleanTitle(title);
+    const blacklisted = await validateTitle(title);
+    if(blacklisted){
+      throw new Error(`Title is blacklisted: ${title}`);
+    }
     //title.replace(/<[^>]*>?/gm, ''); //strip html tags
     if( typeof title === 'string' || title instanceof String){
       var abstracts = $(selectors.abstract).text() ;
