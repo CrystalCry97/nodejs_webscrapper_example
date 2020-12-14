ab41c095c67d39960437235b0af366ccfa383eee
nurbxfit <root.toor97@gmail.com>
2020-12-14 17:01:17 +0800

    update-doi: console log result

--- src/update-doi.js
+++ src/update-doi.js
@@ -46,6 +46,7 @@ const fetchAndGet = async function () {
     //const links = await dbArticles.find({category: {$nin:excludeList}},'link category')
     const n = 20;
     const links = await dbArticles.find({doi: {$exists : false}, category: {$nin:excludeList}},'link category')
+    console.log('Result:',links.length);
     if( links.length > 0 ){
       const split_links = new Array(Math.ceil(links.length/n)).fill().map(_=>links.splice(0,n));
       //console.log('Splitted:',split_links);
